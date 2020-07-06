fbf9544f4ef9b4078e468af1cbdc66de543be3f2
zong-xun <mx91163988@gmail.com>
2020-07-06 19:07:11 +0800

    menu

--- js/share.js
+++ js/share.js
@@ -38,7 +38,7 @@ untouchmove = () => { document.getElementsByClassName('js-navbox_hidden').remove
             // document.body.addEventListener('touchmove', function(event) {
             //     event.preventDefault();
             // }, true); 
-            $('.navbox_menu').on('touchmove', function(event) {
+            $('#navboxId').on('touchmove', function(event) {
                 console.log(event)
                 $('.aa').text('222');
                 event.preventDefault();
@@ -54,7 +54,7 @@ untouchmove = () => { document.getElementsByClassName('js-navbox_hidden').remove
             // $(".js-navbox_hidden").on(' touchend',function(){    
             //     $(".js-navbox_hidden").unbind('touchmove');
             // });
-            $('.navbox_menu').on('touchmove', function(event) {
+            $('#navboxId').on('touchmove', function(event) {
                 $('.aa').text('111');
             });
             document.documentElement.style.overflowY = 'scroll';
