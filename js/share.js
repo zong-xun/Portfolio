c4221df088db47c6458c786b8034a47235dc3898
zong-xun <mx91163988@gmail.com>
2020-07-07 15:31:40 +0800

    MENU

--- js/share.js
+++ js/share.js
@@ -11,51 +11,42 @@ $(function () {
 
     });
 
-    const handler = (e)=>{
-        e.preventDefault();
-    }
-// 防止下拉 
-touchmove= () => { document.getElementsByClassName('js-navbox_hidden').addEventListener('touchmove', handler, false)  }
-// 移除监听，恢复下拉 
-untouchmove = () => { document.getElementsByClassName('js-navbox_hidden').removeEventListener('touchmove', handler, false)  }
 
 
     //控制menu
     $('.js-hamburger').on('click', function () {
         let $navboxId = $('#navboxId');
+        let after = $(window).scrollTop();
         $navboxId.toggleClass('navbox_hidden_hight');
 
         if($navboxId.hasClass('navbox_hidden_hight')){
+            if(after == 0){
+                $('.navbox-hide-height').animate({height:"100%"},500);
+            }
             $navboxId.animate({height:"100%"},500);
-            $('.navbox_menu').animate({opacity:1},400);
-            $('.navbox_menu').removeClass('d-none');
+            $('.navbox_menu').animate({opacity:1},300,()=>{
+                $('.navbox_menu').removeClass('d-none');
+            });
+            
 
-            // $(".js-navbox_hidden").on(' touchstart',function(){    
-            //     $(".js-navbox_hidden").on('touchmove',function(event) {    
-            //         event.preventDefault();     
-            //     }, false);
-            // });
-            // document.body.addEventListener('touchmove', function(event) {
-            //     event.preventDefault();
-            // }, true); 
+            //控制mobile 滾動關必
             $('#navboxId').on('touchmove', function(event) {
-                console.log(event)
-                $('.aa').text('222');
                 event.preventDefault();
             });
             document.documentElement.style.overflowY = 'hidden'; 
         }else{
-            $navboxId.animate({height:"10%"},500,function(){
+
+            if(after == 0){
+                $('.navbox-hide-height').animate({height:"65px"},500);
+            }
+            $navboxId.animate({height:"15%"},500,function(){
                 $('.navbox_menu').addClass('d-none');
                 $navboxId.removeAttr("style");
+                
             });
             $('.navbox_menu').animate({opacity:0},400);
-
-            // $(".js-navbox_hidden").on(' touchend',function(){    
-            //     $(".js-navbox_hidden").unbind('touchmove');
-            // });
+            //控制mobile 滾動開啟
             $('#navboxId').on('touchmove', function(event) {
-                $('.aa').text('111');
             });
             document.documentElement.style.overflowY = 'scroll';
         }
@@ -65,5 +56,27 @@ untouchmove = () => { document.getElementsByClassName('js-navbox_hidden').remove
         $('html,body').animate({ scrollTop: $('#first-block').offset().top }, 800);
     });
 
+    let befort = 0;
+    $(window).on('scroll',()=>{
+        let after = $(window).scrollTop();
+        let differh = after - befort;
+        if(differh == 0){
+            return false;
+        }
+        let scolltype = differh > 0 ?'down' :'up';
+        befort = after;
+        if(scolltype == 'down'){
+            // $('.js-navbox').animate({opacity:"1"},"400");
+            $('.js-navbox').addClass("navbox_position_opacity0");
+            $('.js-navbox').removeClass("navbox_position_opacity1");
+            $('.navbox').addClass('navbox_position');
+        }else{
+            // $('.js-navbox').animate({opacity:"0"},"400");
+            $('.navbox').removeClass('navbox_position');
+            
+            $('.js-navbox').removeClass('navbox_position_opacity0');
+            $('.js-navbox').addClass("navbox_position_opacity1");
+        }
+    })
 
 })
